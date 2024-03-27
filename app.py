3cd3b90ebfef817085e0e4a4ab1fb843ebdec4bc
Fukongfred <fucongxu@uw.edu>
2024-03-26 21:50:30 -0700

    init

--- app.py
+++ app.py
@@ -1,18 +1,51 @@
 import streamlit as st
 
-st.title('Hi! This is Fukong~')
-st.title('_Streamlit_ is :blue[cool] :sunglasses:💜')
+st.title('Hi! This is Fukong~ : sunglasse : 💜')
 
-st.markdown("*Streamlit* is **really** ***cool***.")
-st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors].''')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+pic = st.toggle('See my picture')
+if pic:
+    st.image("C:\Users\86138\Desktop\xhs\Group 1576.png", caption=None, width=2, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
+st.subheader('', divider='rainbow')
 
-Two (or more) newline characters in a row will result in a hard return.
-'''
-st.markdown(multi)
+bar = st.select_slider(
+    'Select a color of the rainbow',
+    options=['About', 'Education', 'Work Experience', 'Hobbies and Interests', 'Interesting Projects'])
+
+if bar == 'About Me':
+    st.subheader('About')
+    about= '''
+    I am working on my **startup**,.
+    '''
+    st.markdown(about)
+
+if bar == 'Education':
+    st.subheader('Education')
+    education= '''
+    - **University of Washington**  2023 - 2025
+    Master of Science in Technology Innovation 💜 @ Golbal Innovation Exchange
+    '''
+    st.markdown(education)
+    
+if bar == 'Work Experience':
+    st.subheader('Work Experience')
+    work= '''
+    - **SCUT HCI Design Lab**   2020 - 2023
+    Researcher and Program Manager
+    '''
+    st.markdown(work)
+
+if bar == 'Hobbies and Interests':
+    st.subheader('Hobbies and Interests')
+    hobbies= '''
+    - 💪🏀🏈🎱🎿🥏🎮🔮🎲🕹️🀄♠️
+    '''
+    st.markdown(hobbies)
+
+if bar == 'Interesting Projects':
+    st.subheader('Interesting Projects')
+    proj= '''
+    - **Project 1** : GAC Metaverse Mobility Competition
+    - **Project 2** : TAURUS Service&UX Redesign
+    '''
+    st.markdown(proj)
